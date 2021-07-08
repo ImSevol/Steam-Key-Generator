3ef9a758562f2c208d540d2e29ef3ad47c83807c
ImSevol <62372284+ImSevol@users.noreply.github.com>
2021-07-08 17:40:31 +0300

    Update RandSymbols.java

--- SteamKeyGen/src/com/steam/keygen/RandSymbols.java
+++ SteamKeyGen/src/com/steam/keygen/RandSymbols.java
@@ -3,14 +3,14 @@ package com.steam.keygen;
 public class RandSymbols {
     public static String RandomCharacter() {
         String ResultCharacter;
-        int Character = (int) (Math.random() * 26);
+        int Character = (int) (Math.random() * MyArrays.ABC.length);
         ResultCharacter = MyArrays.ABC[Character];
         return ResultCharacter;
     }
 
     public static String RandomNumber() {
         String ResultNumber;
-        int Number = (int) (Math.random() * 10);
+        int Number = (int) (Math.random() * MyArrays.NUMBERS.length);
         ResultNumber = MyArrays.NUMBERS[Number];
         return ResultNumber;
     }
