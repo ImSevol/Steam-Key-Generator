07d444253e0a391b89fa68b6c4a44a0f3dc5a0e0
ImSevol <62372284+ImSevol@users.noreply.github.com>
2021-07-09 10:30:15 +0300

    Update MainActivity.java

--- SteamKeyGen/src/com/steam/keygen/MainActivity.java
+++ SteamKeyGen/src/com/steam/keygen/MainActivity.java
@@ -4,23 +4,29 @@ import java.io.*;
 
 public class MainActivity {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         GenerateFile("D:\\", "Generated", ".txt", 2);
-        System.out.println("Key Generated. Try It!");
-    }
-    /*remove lines 17, 18, 20, 21 if long keys generation not needed*/
-    private static void GenerateFile(String directory, String fileName, String format, int keysCount) throws IOException {
-        File file = new File(directory + fileName + format);
-        file.createNewFile();
-        PrintWriter pw = new PrintWriter(file);
-        for (int i = 0; i < keysCount; i++) {
-            int RandomKeyID = (int) (Math.random() * 10 + 1);
-            if (RandomKeyID > 3)
-                pw.println(RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol());
-            else
-                pw.println(RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol());
-        }
-        pw.close();
     }
 
+    /*remove lines 17, 18, 20, 21 if long keys generation not needed*/
+    private static void GenerateFile(String directory, String fileName, String format, int keysCount) {
+        try {
+            File file = new File(directory + fileName + format);
+            file.createNewFile();
+            PrintWriter pw = new PrintWriter(file);
+            for (int i = 0; i < keysCount; i++) {
+                int RandomKeyID = (int) (Math.random() * 10 + 1);
+                if (RandomKeyID > 3)
+                    pw.println(RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol());
+                else
+                    pw.println(RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + "-" + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol() + RandSymbols.RandomSymbol());
+            }
+            pw.close();
+            System.out.println("Key Generated. Try It!");
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found!");
+        } catch (IOException e) {
+            System.out.println("Select Real Directory!");
+        }
+    }
 }
